e94ae3c16c4ed7eb16dcd48b53c3a87c188fc8dd
Scorpio Gentles <ScorpioGentles@outlook.com>
2022-03-31 12:26:33 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Rectangle.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+public class Rectangle {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public boolean isSquare(){
+        return false;
+    }
+
+    public double getArea(){
+        return width*height;
+    }
+
+    public double getPerimeter(){
+        return -1;
+    }
+
+
+}
