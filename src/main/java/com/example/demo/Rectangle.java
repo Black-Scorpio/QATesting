83cc588bb02f58e6de9f621a6f4422727263cd5a
Scorpio Gentles <ScorpioGentles@outlook.com>
2022-03-31 13:00:34 -0400

    Initialized Tests

--- src/main/java/com/example/demo/Rectangle.java
+++ src/main/java/com/example/demo/Rectangle.java
@@ -1,41 +1,72 @@
 package com.example.demo;
 
 public class Rectangle {
-    private double width;
-    private double height;
+    private double width, height;
 
     public Rectangle(double width, double height) {
-        this.width = width;
-        this.height = height;
+        setHeight(height);
+        setWidth(width);
+    }
+
+    public boolean isFunny()
+    {
+        return true;
     }
 
     public double getWidth() {
         return width;
     }
 
+    /**
+     * The width must be greater than 0
+     * @param width
+     */
     public void setWidth(double width) {
-        this.width = width;
+        if (width > 0)
+            this.width = width;
+        else
+            throw new IllegalArgumentException("width must be greater than 0");
     }
 
     public double getHeight() {
         return height;
     }
 
+    /**
+     * The height must be greater than 0
+     * @param height
+     */
     public void setHeight(double height) {
-        this.height = height;
+        if (height > 0)
+            this.height = height;
+        else
+            throw new IllegalArgumentException("Height must be greater than 0");
     }
 
-    public boolean isSquare(){
-        return false;
+    /**
+     * Returns true if the height and width are the same
+     * @return
+     */
+    public boolean isSquare()
+    {
+        return height == width;
     }
 
-    public double getArea(){
-        return width*height;
+    /**
+     * This method returns the area of the rectangle
+     * @return double representing the area
+     */
+    public double getArea()
+    {
+        return height * width;
     }
 
-    public double getPerimeter(){
-        return -1;
+    /**
+     * This is the length of all the sides added up
+     * @return
+     */
+    public double getPerimeter()
+    {
+        return 2*width + 2*height;
     }
-
-
 }
